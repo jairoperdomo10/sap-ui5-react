41243e7c90395c3a9f1dd8c5a3d0cd56eb826379
Jairo Perdomo <jaanpeto_wx@hotmail.com>
2020-08-10 08:05:32 -0500

    final version

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSDk9yl19U_es0CeHlHsEuD1z7u2oPdSg",
+  authDomain: "sap-ui5-aff1b.firebaseapp.com",
+  databaseURL: "https://sap-ui5-aff1b.firebaseio.com",
+  projectId: "sap-ui5-aff1b",
+  storageBucket: "sap-ui5-aff1b.appspot.com",
+  messagingSenderId: "670635957237",
+  appId: "1:670635957237:web:5a8c7cf0ea5b7445e89ebc",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
